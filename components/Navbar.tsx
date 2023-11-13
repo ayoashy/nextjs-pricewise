fecfb9d99fd03badb8bc69007c69450121c5024c
Ayo Ashiru <ashiruayo@gmail.com>
2023-11-13 19:24:09 +0100

    navbar setup

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,10 +2,16 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
+const navIcons = [
+  { src: '/assets/icons/search.svg', alt: 'search' },
+  { src: '/assets/icons/black-heart.svg', alt: 'heart' },
+  { src: '/assets/icons/user.svg', alt: 'user' },
+];
+
 const Navbar = () => {
   return (
-    <header>
-      <nav>
+    <header className='w-full'>
+      <nav className='nav'>
         <Link href={'/'} className='flex items-center gap-1'>
           <Image
             src={'/assets/icons/logo.svg'}
@@ -13,7 +19,23 @@ const Navbar = () => {
             width={27}
             alt='logo'
           />
+          <p className='nav-logo'>
+            Price<span className='text-primary'>wise</span>
+          </p>
         </Link>
+
+        <div className='flex items-center gap-5'>
+          {navIcons.map((icon) => (
+            <Image
+              alt={icon.alt}
+              src={icon.src}
+              key={icon.alt}
+              width={28}
+              height={28}
+              className='object-contain'
+            />
+          ))}
+        </div>
       </nav>
     </header>
   );
