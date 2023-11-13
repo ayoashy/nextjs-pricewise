4ad519dac21eb924378c608a392d81ef72784848
Ayo Ashiru <ashiruayo@gmail.com>
2023-11-13 19:53:47 +0100

    homepage set up

---
+++ components/Searchbar.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { FormEvent, useState } from 'react';
+
+const Searchbar = () => {
+  const [searchPrompt, setSearchPrompt] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const handleSubmit = () => {};
+  return (
+    <form className='flex flex-wrap gap-4 mt-12' onSubmit={handleSubmit}>
+      <input
+        type='text'
+        value={searchPrompt}
+        onChange={(e) => setSearchPrompt(e.target.value)}
+        placeholder='Enter product link'
+        className='searchbar-input'
+      />
+      <button
+        type='submit'
+        className='searchbar-btn'
+        disabled={searchPrompt === ''}
+      >
+        search
+      </button>
+    </form>
+  );
+};
+
+export default Searchbar;
